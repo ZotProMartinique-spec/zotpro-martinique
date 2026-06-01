09bc0b8fcbfb10008bd517a8000d197e85873e01
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-01 15:55:59 -0400

    Remove unused routes from Post.routes.js
    Removed routes for getting a post by ID and deleting a post.

--- Routes/Post.routes.js
+++ Routes/Post.routes.js
@@ -4,32 +4,15 @@ import {
   getPosts,
   toggleLike,
   addComment,
-  deletePost,
-  getPostById,
 } from "../controllers/post.controller.js";
 
 import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-/* ================= PUBLIC FEED ================= */
 router.get("/", getPosts);
-
-/* ================= SINGLE POST ================= */
-router.get("/:id", getPostById);
-
-/* ================= PROTECTED ================= */
-
-/* CREATE POST */
 router.post("/", auth, createPost);
-
-/* LIKE / UNLIKE */
 router.post("/:id/like", auth, toggleLike);
-
-/* COMMENT */
 router.post("/:id/comment", auth, addComment);
-
-/* DELETE POST */
-router.delete("/:id", auth, deletePost);
 
 export default router;
