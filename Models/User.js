60b735db70b9d1ced58197c43bf056b07145acba
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-01 16:06:30 -0400

    Refactor User schema to include followers and following

--- Models/User.js
+++ Models/User.js
@@ -1,7 +1,28 @@
-following: [
-  { type: mongoose.Schema.Types.ObjectId, ref: "User" }
-],
+import mongoose from "mongoose";
 
-followers: [
-  { type: mongoose.Schema.Types.ObjectId, ref: "User" }
-],
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
+
+    password: { type: String, required: true },
+
+    avatar: { type: String, default: "" },
+
+    bio: { type: String, default: "" },
+
+    followers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", UserSchema);
