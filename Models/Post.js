b1ea1b732a60a9791178589fb8a45d4de654b861
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-01 14:57:05 -0400

    Add Post model with schema and timestamps

--- Models/Post.js
+++ Models/Post.js
@@ -1,1 +1,14 @@
+import mongoose from "mongoose";
 
+const PostSchema = new mongoose.Schema(
+  {
+    user: { type: String, required: true },
+    caption: { type: String, required: true },
+    likes: { type: Number, default: 0 }
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model("Post", PostSchema);
+
+export default Post;
