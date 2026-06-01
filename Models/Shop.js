15777d6b3b037c12706ff45488382a14fb594519
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-01 14:54:31 -0400

    Add Shop model with schema and export

--- Models/Shop.js
+++ Models/Shop.js
@@ -1,1 +1,14 @@
+import mongoose from "mongoose";
 
+const ShopSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    description: { type: String }
+  },
+  { timestamps: true }
+);
+
+const Shop = mongoose.model("Shop", ShopSchema);
+
+export default Shop;
