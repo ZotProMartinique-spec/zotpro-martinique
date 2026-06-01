2006c5af64dab36b34b47bdf05343fa9fc497473
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-01 15:50:16 -0400

    Rename model from 'Post' to 'Product' and update schema

--- Models/Product.js
+++ Models/Product.js
@@ -1,23 +1,12 @@
 import mongoose from "mongoose";
 
 export default mongoose.model(
-  "Post",
+  "Product",
   new mongoose.Schema(
     {
-      user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-      content: String,
-      image: String,
-
-      likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-      comments: [
-        {
-          user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-          text: String,
-          createdAt: { type: Date, default: Date.now },
-        },
-      ],
-
-      score: { type: Number, default: 0 },
+      name: String,
+      price: Number,
+      shop: { type: mongoose.Schema.Types.ObjectId, ref: "Shop" },
     },
     { timestamps: true }
   )
