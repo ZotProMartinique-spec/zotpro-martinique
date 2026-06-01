698d6cfe748e947274e9b10b4fd9d3fa34c571f5
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-01 15:43:06 -0400

    Rename Product model to Post and expand schema
    Changed model name from 'Product' to 'Post' and added fields for user, content, image, likes, comments, and score.

--- Models/Product.js
+++ Models/Product.js
@@ -1,12 +1,23 @@
 import mongoose from "mongoose";
 
 export default mongoose.model(
-  "Product",
+  "Post",
   new mongoose.Schema(
     {
-      name: String,
-      price: Number,
-      shop: { type: mongoose.Schema.Types.ObjectId, ref: "Shop" },
+      user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+      content: String,
+      image: String,
+
+      likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+      comments: [
+        {
+          user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+          text: String,
+          createdAt: { type: Date, default: Date.now },
+        },
+      ],
+
+      score: { type: Number, default: 0 },
     },
     { timestamps: true }
   )
