b3f1bd9a63638577c974a008219579d5b8fdd7a4
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-01 15:15:41 -0400

    Add routes for post creation, liking, and commenting

--- Routes/Post.routes.js
+++ Routes/Post.routes.js
@@ -1,1 +1,25 @@
+import express from "express";
+import {
+  createPost,
+  getPosts,
+  toggleLike,
+  addComment,
+} from "../controllers/post.controller.js";
 
+import { auth } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* PUBLIC FEED */
+router.get("/", getPosts);
+
+/* CREATE POST */
+router.post("/", auth, createPost);
+
+/* LIKE / UNLIKE */
+router.post("/:id/like", auth, toggleLike);
+
+/* COMMENT */
+router.post("/:id/comment", auth, addComment);
+
+export default router;
