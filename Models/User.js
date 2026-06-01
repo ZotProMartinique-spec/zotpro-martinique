925edd30ac58eb9f6f66c81399695913b8fde54b
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-01 14:53:38 -0400

    Create User model with schema and export

--- Models/User.js
+++ Models/User.js
@@ -1,1 +1,14 @@
+import mongoose from "mongoose";
 
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    email: { type: String, required: true, unique: true, index: true },
+    password: { type: String, required: true }
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
