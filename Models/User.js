72a35868fc1d563be011334489fc0baf8d033852
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-01 15:58:45 -0400

    Add followers field to User schema
    Added 'followers' field to User schema.

--- Models/User.js
+++ Models/User.js
@@ -1,12 +1,7 @@
-import mongoose from "mongoose";
+following: [
+  { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+],
 
-const UserSchema = new mongoose.Schema(
-  {
-    name: String,
-    email: { type: String, unique: true },
-    password: String,
-  },
-  { timestamps: true }
-);
-
-export default mongoose.model("User", UserSchema);
+followers: [
+  { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+],
