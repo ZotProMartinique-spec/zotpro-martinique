06af13cb574462f386ad952742cf7c3a420492a6
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-01 21:24:32 -0400

    Add User model with schema definition

--- Backend/Models/User.js
+++ Backend/Models/User.js
@@ -1,1 +1,15 @@
+import mongoose from "mongoose";
 
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: { type: String, unique: true },
+    password: String,
+    bio: String,
+    avatar: String,
+    role: { type: String, default: "user" }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema);
