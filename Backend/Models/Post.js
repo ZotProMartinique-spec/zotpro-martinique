8af10f18b80abc349e1b5734d90d31f9a6948d11
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-02 08:50:41 -0400

    Add Post and Comment schemas with Mongoose

--- Backend/Models/Post.js
+++ Backend/Models/Post.js
@@ -1,1 +1,52 @@
+import mongoose from "mongoose";
 
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    content: {
+      type: String,
+      required: true,
+    },
+
+    image: {
+      type: String,
+      default: "",
+    },
+
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    comments: [commentSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Post", postSchema);
