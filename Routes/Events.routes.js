b4b829c1213478e62d0873645a8d0ad3c36ad098
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-02 09:11:29 -0400

    Add event routes for CRUD operations

--- Routes/Events.routes.js
+++ Routes/Events.routes.js
@@ -1,1 +1,25 @@
+import express from "express";
 
+import {
+  createEvent,
+  getEvents,
+  getEvent,
+  joinEvent,
+  deleteEvent,
+} from "../controllers/event.controller.js";
+
+import { auth } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/", getEvents);
+
+router.get("/:id", getEvent);
+
+router.post("/", auth, createEvent);
+
+router.post("/:id/join", auth, joinEvent);
+
+router.delete("/:id", auth, deleteEvent);
+
+export default router;
