ae35793069482cec89e41ae567be866a3c53edd2
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-02 09:09:30 -0400

    Add event schema with validation and defaults

--- Models/Event.js
+++ Models/Event.js
@@ -1,1 +1,58 @@
+import mongoose from "mongoose";
 
+const eventSchema = new mongoose.Schema(
+  {
+    creator: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    title: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      default: "",
+    },
+
+    image: {
+      type: String,
+      default: "",
+    },
+
+    location: {
+      type: String,
+      required: true,
+    },
+
+    startDate: {
+      type: Date,
+      required: true,
+    },
+
+    endDate: {
+      type: Date,
+      required: true,
+    },
+
+    attendees: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    category: {
+      type: String,
+      default: "Autre",
+    }
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Event", eventSchema);
