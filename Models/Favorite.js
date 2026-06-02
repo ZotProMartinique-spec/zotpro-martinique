5257efa4a99b0d397723358fa175041038c205de
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-02 09:06:00 -0400

    Create Favorite model with schema for favorites

--- Models/Favorite.js
+++ Models/Favorite.js
@@ -1,1 +1,27 @@
+import mongoose from "mongoose";
 
+const favoriteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    itemId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+
+    itemType: {
+      type: String,
+      enum: ["shop", "product", "event", "user"],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Favorite", favoriteSchema);
