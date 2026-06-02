9b76a05bb66a2a949a67858103d7b5b86d7cc57f
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-02 09:18:41 -0400

    Add message routing for sending and retrieving messages

--- Routes/Message.routes.js
+++ Routes/Message.routes.js
@@ -1,1 +1,26 @@
+import express from "express";
 
+import {
+  sendMessage,
+  getConversation,
+  getInbox,
+  markAsRead,
+} from "../controllers/message.controller.js";
+
+import { auth } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* SEND */
+router.post("/", auth, sendMessage);
+
+/* INBOX */
+router.get("/inbox", auth, getInbox);
+
+/* CONVERSATION */
+router.get("/:userId", auth, getConversation);
+
+/* READ */
+router.put("/:id/read", auth, markAsRead);
+
+export default router;
