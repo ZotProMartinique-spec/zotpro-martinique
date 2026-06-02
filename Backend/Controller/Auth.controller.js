eef4b746dbc5b64e29744b1391a71ea452444d50
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-01 21:35:01 -0400

    Implement user registration and login functionality

--- Backend/Controller/Auth.controller.js
+++ Backend/Controller/Auth.controller.js
@@ -1,1 +1,40 @@
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
+import { generateToken } from "../utils/generateToken.js";
 
+export const register = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  const exist = await User.findOne({ email });
+  if (exist) return res.status(400).json({ error: "Email exists" });
+
+  const hashed = await bcrypt.hash(password, 10);
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashed
+  });
+
+  res.json({
+    user,
+    token: generateToken(user._id)
+  });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(404).json({ error: "Not found" });
+
+  const ok = await bcrypt.compare(password, user.password);
+
+  if (!ok) return res.status(401).json({ error: "Wrong password" });
+
+  res.json({
+    user,
+    token: generateToken(user._id)
+  });
+};
