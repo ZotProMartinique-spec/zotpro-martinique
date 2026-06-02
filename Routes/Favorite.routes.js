74e1cd78a7aa6d0d2fe67801d0603c769481e011
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-02 09:08:24 -0400

    Add favorite routes for managing favorites

--- Routes/Favorite.routes.js
+++ Routes/Favorite.routes.js
@@ -1,1 +1,19 @@
+import express from "express";
 
+import {
+  addFavorite,
+  getFavorites,
+  removeFavorite,
+} from "../controllers/favorite.controller.js";
+
+import { auth } from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.get("/", auth, getFavorites);
+
+router.post("/", auth, addFavorite);
+
+router.delete("/:id", auth, removeFavorite);
+
+export default router;
