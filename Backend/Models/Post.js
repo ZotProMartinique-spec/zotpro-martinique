31b1ee5b4f04b40845f1af9b226730514d51a5a0
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-03 11:39:06 -0400

    Add Post model with schema for user content

--- Backend/Models/Post.js
+++ Backend/Models/Post.js
@@ -1,1 +1,13 @@
+import mongoose from "mongoose";
 
+const postSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    content: String,
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }]
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Post", postSchema);
