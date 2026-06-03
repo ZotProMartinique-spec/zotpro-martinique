65c597906af49676fdae58ce014a400f8ffca1d8
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-03 12:20:57 -0400

    Add Product model with schema definition

--- Backend/Models/Product.js
+++ Backend/Models/Product.js
@@ -1,1 +1,13 @@
+import mongoose from "mongoose";
 
+const productSchema = new mongoose.Schema(
+  {
+    name: String,
+    price: Number,
+    description: String,
+    shop: { type: mongoose.Schema.Types.ObjectId, ref: "Shop" }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Product", productSchema);
