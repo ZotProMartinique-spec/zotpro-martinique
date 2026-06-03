267ec865776e34aa0cf584f704dfc7c5ba548629
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-03 12:32:07 -0400

    Update Like.routes.js

--- Backend/Routes/Like.routes.js
+++ Backend/Routes/Like.routes.js
@@ -1,1 +1,9 @@
+import express from "express";
+import { toggleLike } from "../controllers/like.controller.js";
+import { auth } from "../middleware/auth.js";
 
+const router = express.Router();
+
+router.post("/:id", auth, toggleLike);
+
+export default router;
