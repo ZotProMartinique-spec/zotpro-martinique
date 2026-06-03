545d9e445764e709b6fdef5269fd0208f7fb7937
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-03 11:36:51 -0400

    Add getUserProfile function to retrieve user data

--- Backend/Controller/User.controller.js
+++ Backend/Controller/User.controller.js
@@ -1,1 +1,8 @@
+import User from "../models/User.js";
 
+export const getUserProfile = async (req, res) => {
+  const user = await User.findById(req.params.id).select("-password");
+  if (!user) return res.status(404).json({ error: "Not found" });
+
+  res.json(user);
+};
