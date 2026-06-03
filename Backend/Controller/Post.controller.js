21d10c2643eacb4848c34b21fadfc3b7289e5da4
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-03 11:35:56 -0400

    Add createPost and getPosts functions

--- Backend/Controller/Post.controller.js
+++ Backend/Controller/Post.controller.js
@@ -1,1 +1,21 @@
+import Post from "../models/Post.js";
+import { calculateScore } from "../utils/feedScore.js";
 
+export const createPost = async (req, res) => {
+  const post = await Post.create({
+    user: req.user.id,
+    content: req.body.content
+  });
+
+  res.json(post);
+};
+
+export const getPosts = async (req, res) => {
+  const posts = await Post.find().populate("user");
+
+  const ranked = posts
+    .map(p => ({ ...p._doc, score: calculateScore(p) }))
+    .sort((a, b) => b.score - a.score);
+
+  res.json(ranked);
+};
