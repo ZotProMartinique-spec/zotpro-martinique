099782a218596b52b00bb210e27cf465a42b46e3
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-03 12:31:17 -0400

    Implement addComment and getComments functions

--- Backend/Controller/Comment.controller.js
+++ Backend/Controller/Comment.controller.js
@@ -1,1 +1,30 @@
+import Comment from "../models/Comment.js";
+import Post from "../models/Post.js";
 
+export const addComment = async (req, res) => {
+  try {
+    const { text } = req.body;
+
+    const comment = await Comment.create({
+      user: req.user.id,
+      post: req.params.id,
+      text
+    });
+
+    await Post.findByIdAndUpdate(req.params.id, {
+      $push: { comments: comment._id }
+    });
+
+    res.json(comment);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const getComments = async (req, res) => {
+  const comments = await Comment.find({ post: req.params.id })
+    .populate("user", "name");
+
+  res.json(comments);
+};
