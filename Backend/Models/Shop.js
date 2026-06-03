8b75ec7b4a6f5283b3fb29a9f16e966bd940328f
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-03 12:20:00 -0400

    Create Shop model with schema and timestamps

--- Backend/Models/Shop.js
+++ Backend/Models/Shop.js
@@ -1,1 +1,11 @@
+import mongoose from "mongoose";
 
+const shopSchema = new mongoose.Schema(
+  {
+    name: String,
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Shop", shopSchema);
