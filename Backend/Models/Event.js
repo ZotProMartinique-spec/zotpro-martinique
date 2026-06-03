7f699dd7d9bdd880418afabc1d70a98efec5b379
ZotPro'Martinique <ZotProMartinique@outlook.fr>
2026-06-03 12:19:21 -0400

    Add Event model with schema and timestamps

--- Backend/Models/Event.js
+++ Backend/Models/Event.js
@@ -1,1 +1,12 @@
+import mongoose from "mongoose";
 
+const eventSchema = new mongoose.Schema(
+  {
+    title: String,
+    date: Date,
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Event", eventSchema);
